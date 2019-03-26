a8db1fa115b7ddd4713664a547ed25bc2486be05
Gdp_Zhora <gdp_zhora@mail.ru>
2019-03-26 20:50:56 +0500

    Добавление контекста задачи

---
+++ TaskFamilyApi/Models/TaskDo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskFamilyApi.Models
+{
+    public class TaskDo
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Notes { get; set; }
+
+        public bool Done { get; set; }
+
+    }
+}
--- TaskFamilyApi/Startup.cs
+++ TaskFamilyApi/Startup.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TaskFamilyApi.Models;
 
 namespace TaskFamilyApi
 {
@@ -25,12 +27,14 @@ namespace TaskFamilyApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TaskDoContext>(opt => opt.UseMySQL(Configuration["Data:TodoString:ConnectionString"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TaskDoContext context)
         {
+            context.Database.EnsureCreated();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
