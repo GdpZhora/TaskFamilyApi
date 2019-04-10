8163ab5391c9adf24a3254babaddbd78243d985f
Gdp_Zhora <gdp_zhora@mail.ru>
2019-04-10 21:16:17 +0500

    Добавление базовых классов

---
+++ TaskFamilyWeb/Models/BaseDoc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskFamilyWeb.Models
+{
+    public class BaseDoc
+    {
+        public int Id { get; set; }
+        public bool Draft { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
