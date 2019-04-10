b6d9d85e3752a45ffd22b5b27b9053e332f6e5b5
Gdp_Zhora <gdp_zhora@mail.ru>
2019-04-10 21:08:50 +0500

    Пересоздание проекта

--- TaskFamilyWeb/Program.cs
+++ TaskFamilyWeb/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TaskFamilyApi
+namespace TaskFamilyWeb
 {
     public class Program
     {
