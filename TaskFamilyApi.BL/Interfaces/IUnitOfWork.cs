540cc86e63ae56dec60ae1a1b1ae5c1a3920dfbd
Gdp_Zhora <gdp_zhora@mail.ru>
2020-07-21 22:20:16 +0500

    Дополнил интерфейсы

---
+++ TaskFamilyApi.BL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskFamilyApi.DL.Entityes;
+
+namespace TaskFamilyApi.BL.Interfaces
+{
+    interface IUnitOfWork : IDisposable
+    {
+        ICatalogRepository<ExpenseItem> ExpenseItems { get; }
+        ICatalogRepository<IncomeItem> IncomeItems { get; }
+        ICatalogRepository<Purse> Purses { get; }
+        IDocumentRepository<Expense> Expenses { get; }
+        IDocumentRepository<Income> Incomes { get; }
+        IDocumentRepository<Transfer> Transfers { get; }
+    }
+}
